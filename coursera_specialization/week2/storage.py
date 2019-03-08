6a13de7bdbfdef0fac14a85ad1574e96f8fe0d03
Maxim YAROVOY <myarovoy@e1r8p2.unit.ua>
2019-03-08 17:18:55 +0200

    change the file storage.py

--- coursera_specialization/week2/storage.py
+++ coursera_specialization/week2/storage.py
@@ -1,13 +1,13 @@
 import os
 import tempfile
-import argparse
 import json
-import sys
+import argparse
 
-storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
+storage_file = os.path.join(tempfile.gettempdir(), 'storage.data')
+
 parser = argparse.ArgumentParser()
-parser.add_argument("--key", "-key_name", dest="key")
-parser.add_argument("--val", "-value", dest="val")
+parser.add_argument('--key', '-key', dest='key')
+parser.add_argument('--val', '-value', dest='val')
 args = parser.parse_args()
 
 val = None
@@ -17,30 +17,35 @@ if args.val:
     else:
         val = args.val
 
-key_val = [args.key, val]
-out = []
+path_ex = os.path.exists(storage_file)
 
-try:
-    with open(storage_path, 'r') as f:
+def create_json(path, key, val):
+    with open(path, 'w') as f:
+        storage = {key: [val]}
+        json.dump(storage, f)
+
+def add_to_json(path, key, val):
+    with open(path, 'r') as f:
         storage = json.load(f)
-        if not args.val:
-            for keyval in storage:
-                if keyval[0] == args.key:
-                    out.append(keyval[1])
-            if not out:
-                print(None)
-            else:
-                print(', '.join(out))
-        else:
-            for keyval in storage:
-                if key_val == keyval:
-                    sys.exit(0)
-            storage.append(key_val)
-            with open(storage_path, 'w') as f1:
-                json.dump(storage, f1, indent=4)
-except:
-    if not val:
-        print(None)
+        if key in storage and val not in storage[key]:
+            storage[key].append(val)
+        elif key not in storage:
+            storage[key] = [val]
+        with open(path, 'w') as f1:
+            json.dump(storage, f1, indent=4)
+
+def read_from_json(path, key):
+    if os.path.exists(path):
+        with open(path, 'r') as f:
+            storage = json.load(f)
+            if key in storage:
+                print((', ').join(storage[key]))
     else:
-        with open(storage_path, 'w') as f:
-            json.dump([[args.key, val]], f, indent=4)
+        print(None)
+
+if not val:
+    read_from_json(storage_file, args.key)
+elif path_ex:
+    add_to_json(storage_file, args.key, val)
+else:
+    create_json(storage_file, args.key, val)
